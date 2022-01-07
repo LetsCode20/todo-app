07e83f751e0943efae8d271df6ed77fd94553c81
devtwins0 <devtwins0@gmail.com>
2022-01-07 17:07:47 +0100

    Add Filter Function To our Todos

--- src/Components/Todo.component.jsx
+++ src/Components/Todo.component.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import * as Md from 'react-icons/md';
 
 const Todo = ({ id, text, complete, toggleComplete }) => {
   return (
     <div
       key={id}
-      style={{ color: !complete ? 'red' : 'green', cursor: 'pointer' }}
+      style={{
+        color: !complete ? 'red' : 'green',
+        cursor: 'pointer',
+        display: 'flex',
+      }}
       onClick={toggleComplete}
     >
       <p>{text}</p>
--- src/Components/TodoList.component.jsx
+++ src/Components/TodoList.component.jsx
@@ -4,6 +4,17 @@ import TodoFrom from './TodoFrom.component';
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
+  const [todosToShow, setTodosToShow] = useState('all');
+
+  let showTodos = [];
+
+  if (todosToShow === 'all') {
+    showTodos = todos;
+  } else if (todosToShow === 'active') {
+    showTodos = todos.filter((todo) => !todo.complete);
+  } else if (todosToShow === 'complete') {
+    showTodos = todos.filter((todo) => todo.complete);
+  }
 
   const addTodo = (todo) => {
     const newTodos = [todo, ...todos];
@@ -22,15 +33,22 @@ const TodoList = () => {
   return (
     <div>
       <TodoFrom onSubmit={addTodo} />
-      {todos &&
-        todos.map((todo) => (
-          <Todo
-            key={todo.id}
-            toggleComplete={() => toggleComplete(todo.id)}
-            text={todo.text}
-            complete={todo.complete}
-          />
-        ))}
+      {showTodos.map((todo) => (
+        <Todo
+          key={todo.id}
+          toggleComplete={() => toggleComplete(todo.id)}
+          text={todo.text}
+          complete={todo.complete}
+        />
+      ))}
+
+      <div>Todos left: {todos.filter((todo) => !todo.complete).length}</div>
+
+      <div>
+        <button onClick={() => setTodosToShow('all')}>All</button>
+        <button onClick={() => setTodosToShow('active')}>Active</button>
+        <button onClick={() => setTodosToShow('complete')}>Complete</button>
+      </div>
     </div>
   );
 };
